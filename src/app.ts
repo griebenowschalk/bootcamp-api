1b338ec5d79abe675815281cc1626d917cb08fe9
Schalk Griebenow <schalkgriebenow@Scalk-Griebenow.local>
2025-08-03 12:00:15 +0200

    Remove config.env from tracking and update gitignore

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+
+// Routes
+import bootcamps from '@/routes/bootcamps';
 
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Mounting routes
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
